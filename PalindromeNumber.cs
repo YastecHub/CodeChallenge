2f18eb956b215219482dfec2e6f6a08428574219
yastec <yasiroyebo@gmail.com>
2024-10-25 00:03:45 +0100

    Updates on Question07

--- PalindromeNumber.cs
+++ PalindromeNumber.cs
@@ -4,20 +4,25 @@ namespace CodeChallenge
     {
         public bool IsPalindrome(int x)
         {
+            // Handle the case where x is negative or if x ends with 0 but is not zero
             if (x < 0 || (x % 10 == 0 && x != 0))
             {
                 Console.WriteLine("False!! , Number is not Palindrome");
             }
+            
+            //initialize the num to be reversed
+            int reversedNo = 0;
 
-            int reversedHalf = 0;
-
-            while (x > reversedHalf)
+            //Reverse the second half of the number i.e from the back
+            while (x > reversedNo)
             {
-                reversedHalf = reversedHalf * 10 + x % 10;
+                reversedNo = reversedNo * 10 + x % 10;
                 x /= 10;
             }
-
-            return x == reversedHalf || x == reversedHalf / 10;
+            
+            //Check if the original number is equal to the reversed number
+            //Or if it is equal to the original number after  removing the middle Num (in case of odd Numbers)
+            return x == reversedNo || x == reversedNo / 10;
         } 
     }
 }
