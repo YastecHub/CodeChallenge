171f5b6d5f1a4ffcc668d2c52204983572ec97ca
yastec <yasiroyebo@gmail.com>
2024-11-01 00:29:22 +0100

    Updates on Q12

--- LongestCommonPrefix.cs
+++ LongestCommonPrefix.cs
@@ -4,19 +4,24 @@ namespace CodeChallenge
     {
         public string FindLongestCommonPrefix(string[] strs)
         {
+            //When the User input is null or not in araar as required in Q
             if (strs == null || strs.Length == 0)
             {
                 Console.WriteLine("Please an array of strings");
             }
-
+            
+            //Start with the first string as the initial prefix
             string prefix = strs[0];
 
             for (int i = 1; i < strs.Length; i++)
             {
+                //while the current string i.e string 2 , does not match the prefix
                 while (strs[1].Length < prefix.Length || !strs[i].Substring(0, prefix.Length).Equals(prefix))
                 {
+                    //Shorten the prefix by one Character
                     prefix = prefix.Substring(0, prefix.Length - 1);
-
+                    
+                    //if no common prefix was found
                     if (prefix == "")
                     {
                         Console.WriteLine("No prefix in the Array of stringc inputed");
